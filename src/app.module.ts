0f2009fa0e40e2a23d2f0063e0b9b0f8224c90bf
Alexandr <alexandrinyutin@gmail.com>
2026-08-12 13:56:53 +0300

    feat: add ConfigModule for environment-based configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MortgageModule } from './mortgage/mortgage.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MortgageModule],
+  imports: [MortgageModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
