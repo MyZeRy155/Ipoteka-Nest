f86443d1a46baf1dbe3a45898d6b7ba30fed0b9a
Alexandr <alexandrinyutin@gmail.com>
2026-08-12 14:18:14 +0300

    feat: connect TypeORM to Postgres via ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MortgageModule } from './mortgage/mortgage.module';
-import { ConfigModule } from '@nestjs/config';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [MortgageModule, ConfigModule.forRoot({isGlobal: true})],
+  imports: [
+      MortgageModule,
+      ConfigModule.forRoot({isGlobal: true}),
+      TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+        })
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
