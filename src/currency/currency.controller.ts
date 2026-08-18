63cb4f583ef26cd2324ded9a18b1d5faeea9f376
Alexandr <alexandrinyutin@gmail.com>
2026-08-18 17:26:13 +0300

    feat: integrate currency exchange rate API with retry, Redis cache and fallback handling

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -1,13 +1,15 @@
-import {Controller, Get, UseGuards,Request } from '@nestjs/common';
+import {Controller, Get, UseGuards, Param} from '@nestjs/common';
 import {AuthGuard} from "../auth/auth.guard";
+import {CurrencyService} from "./currency.service";
 
 @Controller('currency')
 @UseGuards(AuthGuard)
 export class CurrencyController {
+    constructor(private readonly currencyService: CurrencyService) {}
 
-    @Get('currencies')
-    testMethod(@Request() req) {
-        return req.user
+    @Get('currencies/:currency')
+    async getCurrencyRate(@Param('currency') currency: string) {
+        return await this.currencyService.getExchangeCurrencyRate(currency);
     }
 
 }
--- src/currency/currency.module.ts
+++ src/currency/currency.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CurrencyService } from './currency.service';
 import { CurrencyController } from './currency.controller';
+import { HttpModule } from '@nestjs/axios'
+import {CacheModule} from "@nestjs/cache-manager";
+import {ConfigService} from "@nestjs/config";
+import KeyvRedis from "@keyv/redis";
 
 @Module({
+  imports: [
+      HttpModule.register({timeout: 8000}),
+      CacheModule.registerAsync({
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          stores: [new KeyvRedis(`redis://${configService.get('REDIS_HOST')}:${configService.get('REDIS_PORT')}`)]
+        })
+      })
+  ],
   providers: [CurrencyService],
   controllers: [CurrencyController]
 })
