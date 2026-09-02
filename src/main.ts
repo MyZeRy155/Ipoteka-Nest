1f89c12ee6de5afcdb5fe39b6158315a2ee9c98a
Alexandr <alexandrinyutin@gmail.com>
2026-09-02 17:01:43 +0300

    feat(swagger): add bearer-auth scheme to protected endpoints

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .setTitle('IpotekaNest')
     .setVersion('1.0')
     .addServer('http://localhost:3000')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
